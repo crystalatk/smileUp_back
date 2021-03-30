cd0e4adf37f58a22962e7462dfc69d6215e2ff0d
Crystal Atkinson <crystaltayloratkinson@gmail.com>
2021-03-30 16:09:14 -0400

    Ability to update the event table through the edit event page

--- routes/admins.js
+++ routes/admins.js
@@ -13,6 +13,7 @@ router.get("/counttotalvolbyevent", async (req, res) => {
 });
 
 // POSTS
+// Add an Event
 router.post("/addevent", (req, res) => {
   const {
     title,
@@ -30,6 +31,47 @@ router.post("/addevent", (req, res) => {
     alerts,
   } = req.body;
   const response = AdminModel.addEvent(
+    title,
+    date_start,
+    date_stop,
+    location,
+    description,
+    headcount_served_potential,
+    signup_deadline,
+    age_min,
+    min_participants,
+    max_participants,
+    adults_needed,
+    num_adults,
+    alerts
+  );
+  if (response.id) {
+    res.send(response);
+  } else {
+    res.send("Error").status(500);
+  }
+});
+
+// Edit an Event
+router.post("/editevent", (req, res) => {
+  const {
+    event_id,
+    title,
+    date_start,
+    date_stop,
+    location,
+    description,
+    headcount_served_potential,
+    signup_deadline,
+    age_min,
+    min_participants,
+    max_participants,
+    adults_needed,
+    num_adults,
+    alerts,
+  } = req.body;
+  const response = AdminModel.updateEvent(
+    event_id,
     title,
     date_start,
     date_stop,
