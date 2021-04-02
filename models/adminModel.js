0d99e1b8c5c6e85f8a0c7cb6098b6e25e6b76f98
Crystal Atkinson <crystaltayloratkinson@gmail.com>
2021-04-02 09:58:24 -0400

    Added ability for sql query to send back how long someone has been at an event for the checkinVolunteersList page. Also, added check in and check out times to the query select statement.

--- models/adminModel.js
+++ models/adminModel.js
@@ -107,7 +107,7 @@ class Admin {
 
   // Get a List of Volunteers and their Info based on event_id
   static async getVolunteersAttendingEvent(event_id) {
-    const query = `SELECT va.volunteer_id as id, v.first_name, v.last_name FROM volunteer_activities va INNER JOIN volunteers v ON va.volunteer_id = v.id WHERE event_id = '${event_id}';`;
+    const query = `SELECT va.volunteer_id as id, v.first_name, v.last_name, va.check_in_time, va.check_out_time, va.check_out_time - va.check_in_time AS total_time FROM volunteer_activities va INNER JOIN volunteers v ON va.volunteer_id = v.id WHERE event_id = ${event_id};`;
     try {
       const response = await db.result(query);
       return response;
