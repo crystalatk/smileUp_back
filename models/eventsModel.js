14ba8712a30e4334ff5ecee24fbc4d5060c45425
Crystal Atkinson <crystaltayloratkinson@gmail.com>
2021-04-05 13:11:27 -0400

    Logic for created approved and needs approval lists

--- models/eventsModel.js
+++ models/eventsModel.js
@@ -35,6 +35,7 @@ class Events {
     }
   }
 
+  // Get Total Events a Volunteer has Completed by VolunteerID
   static async getTotalEventsById(volunteer_id) {
     try {
       const query = `SELECT COUNT(event_id) FROM volunteer_activities WHERE volunteer_id = ${volunteer_id}
@@ -53,6 +54,80 @@ class Events {
     try {
       const query = `
       SELECT COUNT(id) FROM events WHERE date_stop < NOW() ;`;
+      const response = await db.any(query);
+      console.log("this is the", response);
+      return response;
+    } catch (error) {
+      console.log(error);
+      return error.message;
+    }
+  }
+
+  //   Get the all events approved by not checked in by guardian_id
+  static async getAllApprovedMinorEventsByGuardianID(guardian_id) {
+    try {
+      const query = `
+      SELECT va.volunteer_id, e.title, e.date_start, e.date_stop, e.location, va.event_id, v.first_name, v.last_name 
+        FROM volunteer_activities va 
+          INNER JOIN volunteers v ON va.volunteer_id = v.id 
+          INNER JOIN guardian_child_link gcl ON gcl.volunteer_id = va.volunteer_id 
+          INNER JOIN events e ON va.event_id = e.id 
+            WHERE va.check_in_time IS null AND va.guardian_approval = true AND gcl.guardian_id = ${guardian_id};`;
+      const response = await db.any(query);
+      console.log("this is the", response);
+      return response;
+    } catch (error) {
+      console.log(error);
+      return error.message;
+    }
+  }
+
+  // Get the all events needing approval by not checked in by guardian_id
+  static async getAllNeedsApprovalMinorEventsByGuardianID(guardian_id) {
+    try {
+      const query = `
+      SELECT va.volunteer_id, e.title, e.date_start, e.date_stop, e.location, va.event_id, v.first_name, v.last_name 
+        FROM volunteer_activities va 
+          INNER JOIN volunteers v ON va.volunteer_id = v.id 
+          INNER JOIN guardian_child_link gcl ON gcl.volunteer_id = va.volunteer_id 
+          INNER JOIN events e ON va.event_id = e.id 
+            WHERE va.check_in_time IS null AND va.guardian_approval = false AND gcl.guardian_id = ${guardian_id};`;
+      const response = await db.any(query);
+      console.log("this is the", response);
+      return response;
+    } catch (error) {
+      console.log(error);
+      return error.message;
+    }
+  }
+
+  // Get the all events approved by not checked in by volunteer_id
+  static async getAllApprovedMinorEventsByVolunteerID(volunteer_id) {
+    try {
+      const query = `
+      SELECT va.volunteer_id, e.title, e.date_start, e.date_stop, e.location, va.event_id, v.first_name, v.last_name 
+        FROM volunteer_activities va 
+          INNER JOIN volunteers v ON va.volunteer_id = v.id 
+          INNER JOIN events e ON va.event_id = e.id 
+            WHERE va.check_in_time IS null AND va.guardian_approval = true AND va.volunteer_id = ${volunteer_id};`;
+      const response = await db.any(query);
+      console.log("this is the", response);
+      return response;
+    } catch (error) {
+      console.log(error);
+      return error.message;
+    }
+  }
+
+  // Get the all events needing approval by not checked in by volunteer_id
+  static async getAllNeedsApprovedMinorEventsByVolunteerID(volunteer_id) {
+    try {
+      const query = `
+      SELECT va.volunteer_id, e.title, e.date_start, e.date_stop, e.location, va.event_id, v.first_name, v.last_name 
+        FROM volunteer_activities va 
+          INNER JOIN volunteers v ON va.volunteer_id = v.id 
+          INNER JOIN events e ON va.event_id = e.id 
+            WHERE va.check_in_time IS null AND va.guardian_approval = false AND va.volunteer_id = ${volunteer_id};`;
       const response = await db.any(query);
       console.log("this is the", response);
       return response;
